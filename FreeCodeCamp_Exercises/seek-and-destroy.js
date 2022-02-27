51ba99328c4df459239c0992056b1bad438199f2
JohnCru1976 <94476964+JohnCru1976@users.noreply.github.com>
2022-02-27 13:21:56 +0100

    Seek and  destroy

--- FreeCodeCamp_Exercises/seek-and-destroy.js
+++ FreeCodeCamp_Exercises/seek-and-destroy.js
@@ -21,3 +21,30 @@ function destroyer(arr, ...args) {
   console.log(destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, 
   "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", 
   "hotdog", "grandma", "bugati", "trojan"));
+
+  // SOLUTION 1
+  function destroyer(arr) {
+    let valsToRemove = Object.values(arguments).slice(1);
+  
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < valsToRemove.length; j++) {
+        if (arr[i] === valsToRemove[j]) {
+          delete arr[i];
+        }
+      }
+    }
+    return arr.filter(item => item !== null);
+  }
+
+   // SOLUTION 2
+   function destroyer(arr) {
+    var valsToRemove = Array.from(arguments).slice(1);
+    return arr.filter(function(val) {
+      return !valsToRemove.includes(val);
+    });
+   }
+
+    // SOLUTION 3
+    function destroyer(arr, ...valsToRemove) {
+        return arr.filter(elem => !valsToRemove.includes(elem));
+    }
